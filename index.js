d4ff8c1b990ca74512d43488dcae8a55db61a89b
Yichun Wu <kobo1998@live.com>
2019-09-03 13:36:08 -0400

    Test

--- index.js
+++ index.js
@@ -9,16 +9,26 @@ var root = path.resolve();
 http.createServer(function (req, res) {
   var pathname = url.parse(req.url).pathname;
   var filepath = path.join(root,pathname);
-  fs.stat(filepath,function(err,stats){
-    if(err){
-        res.writeHead(404);
-        res.end("404 Not Found.");
-    }else{
-        res.writeHead(200);
-        fs.createReadStream(filepath).pipe(res);
-    }
-  })
+  // fs.stat(filepath,function(err,stats){
+  //   if(err){
+  //       res.writeHead(404);
+  //       res.end("404 Not Found.");
+  //   }else{
+  //       res.writeHead(200);
+  //       fs.createReadStream(filepath).pipe(res);
+  //   }
+  // })
   var post = "";
+  req.on('request', function(req,res){
+    //var url = request.url
+    response.writeHead(200,{'Content-Type':'text/html'})
+    fs.readFile(filepath,function(err,data){
+      if(err){
+          throw err ;
+      }
+      response.end(data);
+    })
+  });
   req.on('data', function (chunk) {
     post += chunk;
   });
