ab9ef903c2323443f8218020c6738ffe4d2a562d
Yichun Wu <kobo1998@live.com>
2019-09-03 14:46:57 -0400

    Update expressTest.js

--- expressTest.js
+++ expressTest.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var unlencodedParser = bodyParser.unlencoded({extended: false})
+var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 app.use('/public', express.static('public'));
 
