aaf7205cf4e1dcf84093c110ae984f79cddfb299
Yichun Wu <kobo1998@live.com>
2019-09-03 14:01:32 -0400

    Test

--- index.js
+++ index.js
@@ -6,33 +6,35 @@ const querystring = require("querystring");
 
 var root = path.resolve();
 
-http.createServer(function (req, res) {
+var server = http.createServer();
+
+server.on('request', function(req,res){
   var pathname = url.parse(req.url).pathname;
   var filepath = path.join(root,pathname);
-  console.log(filepath);
-  var post = "";
-  req.on('request', function(req,res){
-    //var url = request.url
-    response.writeHead(200,{'Content-Type':'text/html'})
-    console.log("1:"+filepath);
-    fs.readFile(filepath,function(err,data){
-      if(err){
-          throw err ;
-      }
-      response.end(data);
-    })
-  });
-  req.on('data', function (chunk) {
-    post += chunk;
-  });
-  req.on('end', function () {
-    post = querystring.parse(post);
-    if(post.name) {
-      res.write(body.name);
-  } else { 
-      
-  }
-  });
-}).listen(3000, function(){
+  res.writeHead(200,{'Content-Type':'text/html'})
+  fs.readFile(filepath,function(err,data){
+    if(err){
+        throw err ;
+    }
+    response.end(data);
+  })
+});
+server.listen(3000, function(){
   console.log('Listening...')
 });
+
+// (function (req, res) {
+//   req.on('data', function (chunk) {
+//     post += chunk;
+//   });
+//   req.on('end', function () {
+//     post = querystring.parse(post);
+//     if(post.name) {
+//       res.write(body.name);
+//   } else { 
+      
+//   }
+//   });
+// }).listen(3000, function(){
+//   console.log('Listening...')
+// });
