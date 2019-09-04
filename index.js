c765419061b6163ce00d30edbe02bc5e60d57d9d
Yichun Wu <kobo1998@live.com>
2019-09-04 10:02:50 -0400

    1

--- index.js
+++ index.js
@@ -12,10 +12,6 @@ app.use(express.static(__dirname));
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/index.html" );
 })
-// app.post('/play',function(req,res){
-//     console.log("Post get")
-//     res.sendFile(__dirname + '/play.html');
-// })
 
 app.listen(3000, function(){
   console.log("Listening...")
