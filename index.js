c624b09bed9849fe15913009bccc22c35ffd5bf9
Yichun Wu <kobo1998@live.com>
2019-09-03 22:05:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
-var name = require('/name.js');
-var play = require('/play.js')
 
+app.use(express.static('/'));
 
 app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
