00e961756da4b2d0f9dca6b82c5cc3f1427c6a22
Yichun Wu <kobo1998@live.com>
2019-09-04 10:13:57 -0400

    1

--- name.js
+++ name.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
             data : JSON.stringify(data),
             dataType:"json",
             success : function(data) {
-                // $("#name").text(data.name+" ")
-                // $("#date").text(data.date)
+                
             }
         })
     })
--- play.js
+++ play.js
@@ -8,7 +8,8 @@ var jsonParser = bodyParser.json()
 router.post('/',jsonParser,function(req,res){
     console.log("Router get post") //1
     console.log(req.body)
-    res.json()
+    //res.json()
+    res.sendFile( __dirname + "/play.html" );
 })
 
 module.exports = router;
