d9113505d352853dc536a9b6ac7c7ee8eda44ae1
Yichun Wu <kobo1998@live.com>
2019-09-03 22:27:54 -0400

    1

--- script/name.js
+++ script/name.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
     $("#play").submit(function(e){
+        console.log($("#name").val())
         var data = {
             name: $("#name").val(),
             date: Date.now()
         }
+        console.log(data)
         $.ajax({
             type: 'post',
             url: '/name',
