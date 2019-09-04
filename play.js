3d35fb3b25b5d7a2650dd377a11ce5ce2f15e96d
Yichun Wu <kobo1998@live.com>
2019-09-03 23:48:10 -0400

    Update play.js

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 
-router.get('/',function(req,res){
+router.get('/play',function(req,res){
     res.sendFile(path.join(__dirname+'/..'+'/html/ttt.html'));
 
 })
