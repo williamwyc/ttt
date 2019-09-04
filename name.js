5c7405874bddcb2bf2c6e33921d54d96ea163821
Yichun Wu <kobo1998@live.com>
2019-09-04 08:43:53 -0400

    1

--- name.js
+++ name.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
             name: $("#name").val(),
             date: Date.now()
         }
+        console.log("data got")
         $.ajax({
             type: 'post',
             url: '/',
@@ -11,6 +12,7 @@ $(document).ready(function(){
             data : JSON.stringify(data),
             dataType:"json",
             success : function(data) {
+                console.log("data sent");
                 window.location.href='/play'
                 $("#name").text(data.name+" ")
                 $("#date").text(data.date)
