55af86d8af6fee5d51efdc1f305cd197d3a9db90
Yichun Wu <kobo1998@live.com>
2019-09-04 10:21:26 -0400

    1

--- play.js
+++ play.js
@@ -4,12 +4,10 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 var jsonParser = bodyParser.json()
 
-
 router.post('/',jsonParser,function(req,res){
     console.log("Router get post") //1
     console.log(req.body)
-    //res.json()
-    res.sendFile( __dirname + "/play.html" );
+    res.json()
 })
 
 module.exports = router;
