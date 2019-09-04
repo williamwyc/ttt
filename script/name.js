82a93634fca633d5d8385194bc74136da6223180
Yichun Wu <kobo1998@live.com>
2019-09-03 22:23:02 -0400

    Update name.js

--- script/name.js
+++ script/name.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
             name: $("#name").val(),
             date: Date.now()
         }
+        e.preventDefault();
         $.ajax({
             type: 'post',
             url: '/',
             contentType: "application/json;charset=UTF-8",
             data : JSON.stringify(data),
             success : function() {
+                console.log("Success data")
                 $("#name").text(data.name+" ")
                 $("#date").text(data.date)
                 window.location.href("../play.html")
