51391f4998dd010ff044142ac8a0516a5e57b32b
Yichun Wu <kobo1998@live.com>
2019-09-04 19:40:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/ttt', function (req, res) {
     res.sendFile( __dirname + "/index.html" );
 })
 
-app.listen(80, function(){
+app.listen(3000, function(){
   console.log("Listening...")
 })
