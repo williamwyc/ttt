13aea15948c8d8058e31e58df637c969a6831483
Yichun Wu <kobo1998@live.com>
2019-09-05 13:28:17 -0400

    a

---
+++ game.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $.ajax({
+        url:'/ttt/play',
+        type:'post',
+        contentType: "application/json;charset=UTF-8",
+        dataType:'json',
+        success:function(data){
+            $("#name").text(data.name+" ")
+            $("#date").text(data.date)
+        }
+    })
+});
--- play.js
+++ play.js
@@ -14,12 +14,13 @@ router.get('/play',function(req,res){
 
 router.post('/',jsonParser,function(req,res){
     console.log("Router get post")
+    console.log(req.body)
     if(req.body!= null && req.body.date!=null){
         name = req.body.name
         date = req.body.date
     }
     console.log(name,date)
-    res.send("Hello "+name+", "+date)
+    res.write("Hello "+name+", "+date)
     // console.log("Sending File "+path.join(__dirname+'play.html'))
     // res.sendFile(path.join(__dirname+'/play.html'));
 })
