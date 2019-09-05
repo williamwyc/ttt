2c9b8ee85ed04717b9e4dc0b3cccb1118ede0d4d
Yichun Wu <kobo1998@live.com>
2019-09-05 15:35:36 -0400

    a

--- index.js
+++ index.js
@@ -6,25 +6,13 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 var play = require("./play.js")
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 app.use("/ttt", play)
 app.use(express.static(__dirname));
 
 app.get('/ttt', function (req, res) {
     res.sendFile( __dirname + "/index.html" );
-})
-
-app.post('/ttt',jsonParser,function(req,res){
-  console.log("Router get post")
-  console.log(req)
-  console.log(req.body)
-  if(req.body.name!= null){
-      name = req.body.name
-  }
-  return res.json({
-      name: name,
-      date: Date()
-  })
-  
 })
 
 app.listen(3000, function(){
