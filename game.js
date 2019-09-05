22738d9b6b6b783f7cc816aedbac8fb36fc04c81
Yichun Wu <kobo1998@live.com>
2019-09-05 15:16:29 -0400

    Update game.js

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
         contentType: "application/json;charset=UTF-8",
         dataType:'json',
         success:function(data){
+            console.log(data)
             $("#name").text(data.name+" ")
             $("#date").text(data.date)
         }
