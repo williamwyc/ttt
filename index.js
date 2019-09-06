7fefbc3c61e734161dc10813b89968467ac87831
Yichun Wu <kobo1998@live.com>
2019-09-06 05:22:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
-
+var path = require('path');
 var play = require("./play.js")
 
 app.use(bodyParser.urlencoded({ extended: false }))
