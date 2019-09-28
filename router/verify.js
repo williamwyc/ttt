f6f2d2ea6afa77885e60b02c798755a0a3366712
Yichun Wu <kobo1998@live.com>
2019-09-28 15:14:16 -0400

    verify bug

--- router/verify.js
+++ router/verify.js
@@ -14,12 +14,12 @@ router.post('/',jsonParser,function(req,res){
     console.log("Verify")
     data = req.body
     var db = req.app.locals.db
-    db.collection("users").find({'email': data[email]}.toArray(function(err,result){
+    db.collection("users").find({'email': data.email}.toArray(function(err,result){
         if(err){
             res.json({'status':'ERROR'})
         }
         if(result[0].key==data.key||data.key=='abracadabra'){
-            db.collection('users').update({'email': data['email']},{ $set:
+            db.collection('users').update({'email': data.email},{ $set:
                 {
                 'verify': 'true'
                 }
